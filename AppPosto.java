1dc4fb1a6a37c9b5363e6066324a9dda05aed4ac
fpinheiro1 <39119897+fpinheiro1@users.noreply.github.com>
2018-10-02 21:59:44 -0300

    Trabalho Posto

---
+++ AppPosto.java
@@ -0,0 +1,13 @@
+package appposto;
+
+
+public class AppPosto {
+
+    
+    public static void main(String[] args) {
+    FrmPosto frm = new FrmPosto(); 
+    Combustivel c1 = new Combustivel();
+    
+    }
+    
+}
